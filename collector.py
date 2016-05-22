baebaad6f59856fa0e2c42c5c21930f34b2aad79
rickmur <rick@mur.nl>
2016-05-22 17:10:39 +0200

    First commit

---
+++ collector.py
@@ -0,0 +1,3 @@
+# Rick Mur - Maverick Solutions - (c) 2016
+# This script receives JSON data over HTTP and stores in InfluxDB
+
---
+++ querier.py
@@ -0,0 +1,5 @@
+# Rick Mur - Maverick Solution - (c) 2016
+# This script queries HTTP/HTTPS or Socket connections and checks if they are alive
+# After the check it sends the results over HTTP to the collector to store in InfluxDB
+
+
